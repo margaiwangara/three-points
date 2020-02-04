42845a8605428b64a9f77ed2922eb5579ee6075d
margaiwangara <margaiwangara@gmail.com>
2020-02-04 12:19:15 +0300

    modified eslint config file, initialized express app, added config example file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import express, { Application } from 'express';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 
-const app = express();
+// Config
+dotenv.config({ path: path.resolve(__dirname, '../config/config.env') });
+
+// Init App
+const app: Application = express();
+
+// PORT
+const PORT: number = parseInt(`${process.env.PORT}`, 10) || 5005;
+
+app.listen(PORT, () =>
+  console.log(`App running in ${process.env.NODE_ENV} mode on port ${PORT}`),
+);
